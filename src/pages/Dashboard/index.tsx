2d157b1b8ac9b77bd7bcce0adf8dc79e9f247e11
Renato Castro <renatocastro02@hotmail.com>
2020-10-10 13:57:32 -0300

    Página de perfil

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import { FiClock, FiPower } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
@@ -142,7 +143,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
